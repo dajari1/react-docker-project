6fba3c972ebba0ad035c3334144c006e1c2c2545
unknown <ajaridennis@gmail.com>
2024-12-21 10:26:08 -0500

    reactproject

---
+++ command.md
@@ -0,0 +1,10 @@
+# To check the version 
+node --version
+# To initialize your react app
+npm init -y
+# Command to install the express.js
+npm install express --save
+# To run node application
+node index.js
+# To run in browser
+http://localhost:3000/ok
---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Welcome to my ayeni clement website" }');
+});
+
+app.get('/next', function (req, res) {
+    res.send('{ "response": "yes this is the next page,please register your account"}');
+});
+app.get('/ok', function (req, res) {
+    res.send('{ "response": " Nice Job!, cool job and welldone!" }');
+});
+app.listen(process.env.PORT || 3000);
+console.log(`Server is running on http://0.0.0.0:3000`);
+module.exports = app;
